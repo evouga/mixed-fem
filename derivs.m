dec7ecde2c8f94df611eefb1cf5d75c446dd43d9
Ty Trusty <trusty@trustymacbook.local>
2022-04-04 11:49:53 -0400

    arap and corot

--- derivs.m
+++ derivs.m
@@ -1,25 +1,17 @@
 d=3;
 R=sym('R',[d d]);
-S = sym('S',[6,1]);
-% S = sym('S',[d,d]);
+s = sym('S',[6,1]);
 L = sym('L',[9,1]);
 syms mu la
 assume(R,'real')
-assume(S,'real')
+assume(s,'real')
 assume(mu,'real')
 assume(L,'real')
 
-F = [S(1) S(6) S(5);
-     S(6) S(2) S(4);
-     S(5) S(4) S(3)];
- F = [S(1) S(4) S(5);
-     S(4) S(2) S(6);
-     S(5) S(6) S(3)];
- %F=S;
- %R=eye(3);
- t=13;
-%  R = [cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1]
-R'*R
+S = [s(1) s(4) s(5);
+     s(4) s(2) s(6);
+     s(5) s(6) s(3)];
+
 W = [                          
   [R(1,1) 0 0 0 R(1,3) R(1,2)]
   [0 R(1,2) 0 R(1,3) 0 R(1,1)]
@@ -32,24 +24,12 @@ W = [
   [0 0 R(3,3) R(3,2) R(3,1) 0]
   ];
 
-% W = [                          
-%   [R(1,1) 0 0 0 R(1,3) R(1,2)]
-%   [R(2,1) 0 0 0 R(2,3) R(2,2)]
-%   [R(3,1) 0 0 0 R(3,3) R(3,2)]
-%   [0 R(1,2) 0 R(1,3) 0 R(1,1)]
-%   [0 R(2,2) 0 R(2,3) 0 R(2,1)]
-%   [0 R(3,2) 0 R(3,3) 0 R(3,1)]
-%   [0 0 R(1,3) R(1,2) R(1,1) 0]
-%   [0 0 R(2,3) R(2,2) R(2,1) 0]
-%   [0 0 R(3,3) R(3,2) R(3,1) 0]];
-
-% % stable neohookean
-%F=R*F; (determinant of R=1, F'*F = S'S)
-I3=det(F);
-I2=trace(F'*F);
+% stable neohookean
+I3=det(S);
+I2=trace(S'*S);
 snh= 0.5*mu*(I2-d)- mu*(I3-1)+ 0.5*la*(I3-1)^2;
-H=simplify(hessian(snh,S(:)));
-g=simplify(gradient(snh,S(:)));
+H=simplify(hessian(snh,s(:)));
+g=simplify(gradient(snh,s(:)));
 ccode(snh)
 ccode(H)
 ccode(g)
@@ -61,27 +41,16 @@ ccode(g)
 %snh=0.5*mu*(I3-3)+ 0.5*la*(J-1)^2;
 
 % Corotational material model
-arap= mu*0.5*trace( (F - eye(d))*(F - eye(d))');
-corot=  1*la*0.5*trace(F-eye(d))^2 + 2*arap;
-H=hessian(corot,S(:));
-g=gradient(corot,S(:));
-%ccode(H)
-%ccode(g)
+arap= mu*0.5*trace( (S - eye(d))*(S - eye(d))');
+H=hessian(arap,s(:));
+g=gradient(arap,s(:));
+ccode(arap)
+ccode(H)
+ccode(g)
 
-Hinv = inv(H);
-%Hinv = sym('Hinv',[6,6]);
-%assume(Hinv,'real');
-
-WHW=W*Hinv*W';
-
-% Wst - WH^-1g
-Ws_Hinvg = W*(S-Hinv*g);
-
-% -H^-1*g + H^-1*W'*lambda
-ds = Hinv*(W'*L - g);
-
-%ccode(Hinv)
-
-ccode(WHW)
-ccode(Ws_Hinvg)
-ccode(ds)
+corot = la*0.5*trace(S-eye(d))^2 + 2*arap;
+H=hessian(corot,s(:));
+g=gradient(corot,s(:));
+ccode(corot)
+ccode(inv(H))
+ccode(g)
--- src/materials/arap_model.cpp
+++ src/materials/arap_model.cpp
@@ -27,15 +27,14 @@ void ArapModel::update_compliance(int n, int m,
 double ArapModel::energy(const Vector6d& S) {
     
   double mu = config_->mu;
-  double la = config_->la;
   double S1 = S(0);
   double S2 = S(1);
   double S3 = S(2);
   double S4 = S(3);
   double S5 = S(4);
   double S6 = S(5);
-  std::cerr << "ArapModel::energy not implemented!";
-  return 0.0;
+  return (mu*(pow(S1-1.0,2.0)+pow(S2-1.0,2.0)+pow(S3-1.0,2.0)+(S4*S4)*2.0
+        +(S5*S5)*2.0+(S6*S6)*2.0))/2.0;
 }
 
 Vector6d ArapModel::gradient(const Matrix3d& R,
@@ -50,12 +49,12 @@ Vector6d ArapModel::gradient(const Matrix3d& R,
   double S5 = S(4);
   double S6 = S(5);
   Vector6d g;
-  g(0) = S1*mu-mu*(S2*S3-S6*S6)-la*(S2*S3-S6*S6)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
-  g(1) = S2*mu-mu*(S1*S3-S5*S5)-la*(S1*S3-S5*S5)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
-  g(2) = S3*mu-mu*(S1*S2-S4*S4)-la*(S1*S2-S4*S4)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
-  g(3) = S4*mu*2.0+mu*(S3*S4*2.0-S5*S6*2.0)+la*(S3*S4*2.0-S5*S6*2.0)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
-  g(4) = S5*mu*2.0+mu*(S2*S5*2.0-S4*S6*2.0)+la*(S2*S5*2.0-S4*S6*2.0)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
-  g(5) = S6*mu*2.0+mu*(S1*S6*2.0-S4*S5*2.0)+la*(S1*S6*2.0-S4*S5*2.0)*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0);
+  g(0) = (mu*(S1*2.0-2.0))/2.0;
+  g(1) = (mu*(S2*2.0-2.0))/2.0;
+  g(2) = (mu*(S3*2.0-2.0))/2.0;
+  g(3) = S4*mu*2.0;
+  g(4) = S5*mu*2.0;
+  g(5) = S6*mu*2.0;
   return g;
 
 }
@@ -64,55 +63,21 @@ Matrix6d ArapModel::hessian_inv(const Matrix3d& R,
     const Vector6d& S) {
   
   double mu = config_->mu;
-  double la = config_->la;
   double S1 = S(0);
   double S2 = S(1);
   double S3 = S(2);
   double S4 = S(3);
   double S5 = S(4);
   double S6 = S(5);
-  Matrix6d H;
-  H(0,0) = mu+la*pow(S2*S3-S6*S6,2.0);
-  H(0,1) = -S3*mu-S3*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S3-S5*S5)*(S2*S3-S6*S6);
-  H(0,2) = -S2*mu-S2*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S2-S4*S4)*(S2*S3-S6*S6);
-  H(0,3) = -la*(S3*S4*2.0-S5*S6*2.0)*(S2*S3-S6*S6);
-  H(0,4) = -la*(S2*S5*2.0-S4*S6*2.0)*(S2*S3-S6*S6);
-  H(0,5) = S6*mu*2.0+S6*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S1*S6*2.0-S4*S5*2.0)*(S2*S3-S6*S6);
-  H(1,0) = -S3*mu-S3*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S3-S5*S5)*(S2*S3-S6*S6);
-  H(1,1) = mu+la*pow(S1*S3-S5*S5,2.0);
-  H(1,2) = -S1*mu-S1*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S2-S4*S4)*(S1*S3-S5*S5);
-  H(1,3) = -la*(S3*S4*2.0-S5*S6*2.0)*(S1*S3-S5*S5);
-  H(1,4) = S5*mu*2.0+S5*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S2*S5*2.0-S4*S6*2.0)*(S1*S3-S5*S5);
-  H(1,5) = -la*(S1*S6*2.0-S4*S5*2.0)*(S1*S3-S5*S5);
-  H(2,0) = -S2*mu-S2*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S2-S4*S4)*(S2*S3-S6*S6);
-  H(2,1) = -S1*mu-S1*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)+la*(S1*S2-S4*S4)*(S1*S3-S5*S5);
-  H(2,2) = mu+la*pow(S1*S2-S4*S4,2.0);
-  H(2,3) = S4*mu*2.0+S4*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S3*S4*2.0-S5*S6*2.0)*(S1*S2-S4*S4);
-  H(2,4) = -la*(S2*S5*2.0-S4*S6*2.0)*(S1*S2-S4*S4);
-  H(2,5) = -la*(S1*S6*2.0-S4*S5*2.0)*(S1*S2-S4*S4);
-  H(3,0) = -la*(S3*S4*2.0-S5*S6*2.0)*(S2*S3-S6*S6);
-  H(3,1) = -la*(S3*S4*2.0-S5*S6*2.0)*(S1*S3-S5*S5);
-  H(3,2) = S4*mu*2.0+S4*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S3*S4*2.0-S5*S6*2.0)*(S1*S2-S4*S4);
-  H(3,3) = mu*2.0+S3*mu*2.0+la*pow(S3*S4*2.0-S5*S6*2.0,2.0)+S3*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0;
-  H(3,4) = S6*mu*-2.0-S6*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S2*S5*2.0-S4*S6*2.0)*(S3*S4*2.0-S5*S6*2.0);
-  H(3,5) = S5*mu*-2.0-S5*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S1*S6*2.0-S4*S5*2.0)*(S3*S4*2.0-S5*S6*2.0);
-  H(4,0) = -la*(S2*S5*2.0-S4*S6*2.0)*(S2*S3-S6*S6);
-  H(4,1) = S5*mu*2.0+S5*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S2*S5*2.0-S4*S6*2.0)*(S1*S3-S5*S5);
-  H(4,2) = -la*(S2*S5*2.0-S4*S6*2.0)*(S1*S2-S4*S4);
-  H(4,3) = S6*mu*-2.0-S6*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S2*S5*2.0-S4*S6*2.0)*(S3*S4*2.0-S5*S6*2.0);
-  H(4,4) = mu*2.0+S2*mu*2.0+la*pow(S2*S5*2.0-S4*S6*2.0,2.0)+S2*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0;
-  H(4,5) = S4*mu*-2.0-S4*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S1*S6*2.0-S4*S5*2.0)*(S2*S5*2.0-S4*S6*2.0);
-  H(5,0) = S6*mu*2.0+S6*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0-la*(S1*S6*2.0-S4*S5*2.0)*(S2*S3-S6*S6);
-  H(5,1) = -la*(S1*S6*2.0-S4*S5*2.0)*(S1*S3-S5*S5);
-  H(5,2) = -la*(S1*S6*2.0-S4*S5*2.0)*(S1*S2-S4*S4);
-  H(5,3) = S5*mu*-2.0-S5*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S1*S6*2.0-S4*S5*2.0)*(S3*S4*2.0-S5*S6*2.0);
-  H(5,4) = S4*mu*-2.0-S4*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0+la*(S1*S6*2.0-S4*S5*2.0)*(S2*S5*2.0-S4*S6*2.0);
-  H(5,5) = mu*2.0+S1*mu*2.0+la*pow(S1*S6*2.0-S4*S5*2.0,2.0)+S1*la*(S1*(S6*S6)+S2*(S5*S5)+S3*(S4*S4)-S1*S2*S3-S4*S5*S6*2.0+1.0)*2.0;
-
-  //sim::simple_psd_fix(H);
-
-  return H.inverse();
-      
+  Matrix6d Hinv;
+  Hinv.setZero();
+  Hinv(0,0) = 1. / mu;
+  Hinv(1,1) = 1. / mu;
+  Hinv(2,2) = 1. / mu;
+  Hinv(3,3) = 1. / (2.0 * mu);
+  Hinv(4,4) = 1. / (2.0 * mu);
+  Hinv(5,5) = 1. / (2.0 * mu);
+  return Hinv;
 }
 
 Matrix9d ArapModel::WHinvW(const Matrix3d& R,
