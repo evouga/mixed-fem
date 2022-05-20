13690b67d3336f37b9c7f664f406e9ae269c0d48
David Levin <dilevin@csail.mit.edu>
2022-05-20 14:41:21 -0400

    fung

---
+++ src/materials/fung.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "materials/material_model.h"
+
+namespace mfem {
+
+  // Standard neohookean material model
+  class Fung : public MaterialModel {
+  public:
+    
+    Fung(const std::shared_ptr<MaterialConfig>& config)
+        : MaterialModel("Fung", config) {}
+
+    double energy(const Eigen::Vector6d& S) override; 
+    Eigen::Vector6d gradient(const Eigen::Vector6d& S) override; 
+    Eigen::Matrix6d hessian(const Eigen::Vector6d& S,
+        bool psd_fix = true) override;
+
+    double energy(const Eigen::Vector9d& F) override;
+    Eigen::Vector9d gradient(const Eigen::Vector9d& F) override;
+    Eigen::Matrix9d hessian(const Eigen::Vector9d& F) override;
+  };
+
+}
