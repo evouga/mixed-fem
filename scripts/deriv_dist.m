f4ad23757b5f2cd0656aef53fd40f68f12b48bb4
ty <tytrusty@utexas.edu>
2022-08-16 19:28:47 -0400

    blah

--- scripts/deriv_dist.m
+++ scripts/deriv_dist.m
@@ -6,15 +6,25 @@ a = q(1:d);
 b = q(d+1:2*d);
 p = q(2*d+1:3*d);
 
-e = b-a;
-normal = e;
-normal(1) = e(2);
-normal(2) = -e(1);
-normal = normal / norm(normal);
-d = dot(p-a, normal);
-g=gradient(d,q);
-ccode(g)
+% e = b-a;
+% normal = e;
+% normal(1) = e(2);
+% normal(2) = -e(1);
+% normal = normal / norm(normal);
+% d = dot(p-a, normal);
+v = b - a;
+w = p - a;
 
+c1 = dot(w,v);
+c2 = dot(v,v);
+assume(c2 > 0)
+c = c1 / c2;
+d = norm(p - (a + c*v));
+
+
+g=gradient(d,q);
+H=hessian(d,q);
+matlabFunction(g, 'vars', {q},'File','gradient','Optimize',true)
 syms d h k
 psi = -k * (d-h)^2 * log(d/h);
 %psi = (k/h/h)*(d-h)^2 * exp(-d/(h^2));
