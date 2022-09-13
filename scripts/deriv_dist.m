b87ae194074569ace2fe92547c372cfe30f2d544
Ty Trusty <trusty@trustymacbook.local>
2022-09-13 16:21:30 -0400

    update deriv dist script

--- scripts/deriv_dist.m
+++ scripts/deriv_dist.m
@@ -2,25 +2,44 @@ d=2;
 q = sym('q',[d*3,1]);
 assume(q,'real');
 
-a = q(1:d);
-b = q(d+1:2*d);
-p = q(2*d+1:3*d);
+% a = q(1:d);
+% b = q(d+1:2*d);
+% p = q(2*d+1:3*d);
+% 
+% v = b - a;
+% w = p - a;
+% 
+% c1 = dot(w,v);
+% c2 = dot(v,v);
+% assume(c2 > 0)
+% c = c1 / c2;
+% d = norm(p - (a + c*v));
 
-v = b - a;
-w = p - a;
+d=2;
+q = sym('q',[d*3,1]);
+assume(q,'real');
 
-c1 = dot(w,v);
-c2 = dot(v,v);
-assume(c2 > 0)
-c = c1 / c2;
-d = norm(p - (a + c*v));
+p = q(1:d);
+e0 = q(d+1:2*d);
+e1 = q(2*d+1:3*d);
+
+% 2D
+e = e1 - e0;
+num = e(2)*p(1) - e(1)*p(2) + e1(1)*e0(2) - e1(2)*e0(1);
+d = sqrt(num * num / dot(e,e));
+
+% 3D
+% tmp = cross(e0-p,e1-p);
+% e = e1 - e0;
+% d = sqrt(dot(tmp,tmp) / dot(e,e));
+
 
 g=gradient(d,q);
 H=hessian(d,q);
-%ccode(simplify(g))
-%ccode(simplify(H))
-%matlabFunction(g, 'vars', {q},'File','gradient','Optimize',true)
-%matlabFunction(H, 'vars', {q},'File','hessian','Optimize',true)
+% ccode(simplify(g))
+% ccode(simplify(H))
+matlabFunction(g, 'vars', {q},'File','gradient','Optimize',true)
+matlabFunction(H, 'vars', {q},'File','hessian','Optimize',true)
 %%%%%%%%%%%%%%
 d=2;
 q = sym('q',[d*2,1]);
